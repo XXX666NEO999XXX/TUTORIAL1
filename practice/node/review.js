ee27b0d721e4753f768bdeb374e814c8c7b05c32
student <student@PATC21.local>
2019-10-21 10:22:29 -0700

    m

---
+++ practice/node/review.js
@@ -0,0 +1,13 @@
+// Storing things in JavaScript.
+
+var nameOfVar = 1000;
+
+//JavaScript DataTypes
+
+// String, ALWAYS wrapped in single or double matching quotes.
+"Hello there!";
+
+//Number
+55903.33872
+1238899
+-8275618; -1 * 32003;
